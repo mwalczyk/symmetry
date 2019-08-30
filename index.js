2bdd6347ff33f78c238b74e5ab756efa0ded439b
Michael Walczyk <mwalczyk2@gmail.com>
2019-08-30 12:14:35 -0700

    Adds additional comments.

--- index.js
+++ index.js
@@ -6,18 +6,25 @@
 // 2. https://javascript.info/
 // 3.
 
+// this should work in es6 (something wrong with browserify?)
 //import * as dat from 'dat.gui';
 const dat = require('dat.gui');
 
+// setup the gui
 let controls = {
   background: '#000000',
-  modifyStroke: true,
+  animateStroke: true,
+  applyShadows: true,
+  hsvBrush: false,
   rotations: 6
 };
 let gui = new dat.GUI();
-gui.addColor(controls, 'background');
-gui.add(controls, 'modifyStroke')
-gui.add(controls, 'rotations', 3, 50, 1);
+const controller0 = gui.addColor(controls, 'background');
+const controller1 = gui.add(controls, 'animateStroke');
+const controller2 = gui.add(controls, 'applyShadows');
+const controller3 = gui.add(controls, 'hsvBrush');
+const controller4 = gui.add(controls, 'rotations', 3, 50, 1);
+controller2.onChange(setDrawState);
 
 // create canvas element and append it to document body
 const canvas = document.createElement('canvas');
@@ -26,13 +33,13 @@ document.body.appendChild(canvas);
 // get canvas 2D context and set to correct size
 const ctx = canvas.getContext('2d');
 resize();
+setDrawState();
 
 // set up draw state
 let position = { x: 0, y: 0 };
 const strokeSizeBounds = { min: 1, max: 10 }
 let hue = 0;
 let direction = true;
-let totalDistance = 0;
 
 window.addEventListener('resize', resize);
 canvas.addEventListener('mousemove', draw);
@@ -43,16 +50,20 @@ document.addEventListener('keypress', clear);
 function resize() {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
-  ctx.strokeStyle = '#BADA55';
+}
+
+function setDrawState() {
 	ctx.lineJoin = 'round';
 	ctx.lineCap = 'round';
-	//ctx.lineWidth = 20;
-	//ctx.filter = 'blur(8px)';
 
-	ctx.shadowOffsetX = 2;
-  ctx.shadowOffsetY = 2;
-  ctx.shadowBlur = 28;
-  ctx.shadowColor = 'rgba(0, 0, 0, 1.0)';
+	if (controls.applyShadows) {
+		ctx.shadowOffsetX = 2;
+	  ctx.shadowOffsetY = 2;
+	  ctx.shadowBlur = 28;
+	  ctx.shadowColor = 'rgba(0, 0, 0, 1.0)';
+	} else {
+		ctx.shadowColor = 'rgba(0, 0, 0, 0.0)';
+	}
 }
 
 function clear() {
@@ -60,8 +71,7 @@ function clear() {
 }
 
 function distance(first, second) {
-  dx = second.x - first.x
-  dy = second.y - first.y
+  [dx, dy] = [second.x - first.x, second.y - first.y];
   return Math.sqrt(dx * dx + dy * dy)
 }
 
@@ -70,8 +80,10 @@ function setPosition(e) {
 }
 
 function draw(e) {
+	// set the current background color
 	canvas.style.backgroundColor = controls.background; 
 
+	// is the left mouse button pressed?
 	if (e.buttons !== 1) return;
 
 	hue++;
@@ -79,10 +91,15 @@ function draw(e) {
 		hue = 0;
 	}
 
-	ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
-	ctx.strokeStyle = '#FFFFFF';
+	// choose brush color
+	if (controls.hsvBrush) {
+		ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+	} else {
+		ctx.strokeStyle = '#FFFFFF';
+	}
 
-	if (controls.modifyStroke) {
+	// choose whether or not to animate the line width as the user draws
+	if (controls.animateStroke) {
 			if (ctx.lineWidth >= strokeSizeBounds.max || ctx.lineWidth <= strokeSizeBounds.min) {
 				direction = !direction;
 			}
@@ -92,11 +109,13 @@ function draw(e) {
 				ctx.lineWidth--;
 			}
 	} else {
-		ctx.lineWidth = strokeSizeBounds.max;
+		ctx.lineWidth = strokeSizeBounds.max - 1;
 	}
 
+	// draw patterns
   drawLineWithSymmetry(position, e);
 
+  // save cursor position
   setPosition(e);
 }
 
@@ -121,37 +140,58 @@ function drawLineWithSymmetry(start, end) {
 
 	const mirror = false;
 
+	// mirroring the drawing with odd-numbered rotational symmetry (i.e. 3) is interesting
 	if (mirror) {
+		// flip y
 		ctx.save();
 		ctx.setTransform(1, 0, 0, -1, 0, canvas.height);
 		drawLine(start, end);
 		ctx.restore();
 
+		// flip x
 		ctx.save();
 		ctx.setTransform(-1, 0, 0, 1, canvas.width, 0);
 		drawLine(start, end);
 		ctx.restore();
 
+		// flip x and y
 		ctx.save();
 		ctx.setTransform(-1, 0, 0, -1, canvas.width, canvas.height);
 		drawLine(start, end);
 		ctx.restore();
-	} else {
-		const divisions = controls.rotations;
-		const theta = (Math.PI * 2.0) / divisions;
-		const cx = canvas.width * 0.5;
-		const cy = canvas.height * 0.5;
-		
-		for (let i = 0; i < divisions; i++) {
+	} 
+
+	// rotational symmetry
+	const divisions = controls.rotations;
+	const theta = (Math.PI * 2.0) / divisions;
+	const cx = canvas.width * 0.5;
+	const cy = canvas.height * 0.5;
+	
+	for (let i = 0; i < divisions; i++) {
+		// randomly drop certain lines
+		//if (Math.random() > 0.5) {
 			ctx.save();
+
+			// apply rotation around origin
 			ctx.translate(cx, cy);
 			ctx.rotate(theta * i);
 			ctx.translate(-cx, -cy);
 			drawLine(start, end);
 
 			ctx.restore();
-		}
+		//}
 	}
 
+	// ctx.filter = 'blur(8px)';
+	// ctx.stroke();
+	// ctx.filter = 'blur(0px)';
 	ctx.stroke();
 }
+
+function fadeDrawing() {
+	ctx.fillStyle = "rgba(0.0, 0.0, 0.0, 0.005)";
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+// uncomment to slowly clear the canvas as the user draws
+//setInterval(fadeDrawing, 10);
